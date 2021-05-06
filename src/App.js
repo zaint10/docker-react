a1c869bc27a55f4d0219ad6ba0b3c095676ee634
Zain Tanveer <zat938@gmail.com>
2021-05-06 22:47:47 +0500

    Initial Build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simple Docker React App created by Me
+          @author: Zain Tanveer
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Docker from Dev to Production
         </a>
       </header>
     </div>
