2122172c8a2de9d9262145c6e1b2a051d7cd4340
Zain Tanveer <zat938@gmail.com>
2021-05-07 21:26:21 +0500

    :bug: Fix Text

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn docker/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
